a8f0e44c845ac14cee8c6fbf7d10bf5f3acbb33d
diana sazanchuk <dianasazanchuk@gmail.com>
2020-02-02 23:43:33 +0300

    LAB 3: add router

--- lab3/lab3react/src/App.js
+++ lab3/lab3react/src/App.js
@@ -5,19 +5,24 @@ import Routes from './components/route/Routes';
 import TransportList from './components/transport/TransportList';
 import InputTransport from './components/transport/InputTransport';
 import InputRoute from './components/route/InputRoute';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <Home/>
-        <Routes/>
-        <TransportList/>
-        <InputRoute/>
-        <InputTransport/>
-        {/* Логика переключения компонентов */}
-        </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="container" >
+          <Route path="/" exact component={Home}/>
+          <Route strict path="/routes" exact component={Routes} />
+          <Route strict path="/transports" exact component={TransportList} />
+          <Route path="/routes/add" exact component={InputRoute} />
+          <Route path="/transports/add" exact component={InputTransport} />
+        </div>
+      </div>
+    </Router>
+   
   );
 }
 
